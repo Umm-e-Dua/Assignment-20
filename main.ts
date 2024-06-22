c2bb7c0efeb659fbfdaad1cff0cfa0a161f0e5a2
Umm-e-Dua <160483094+Umm-e-Dua@users.noreply.github.com>
2024-06-23 01:39:33 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,4 @@
+// 20 Assignment 
+let programingLanguage = ["Typescript", "Javascript" ,"Python","Html" ];
+console.log("list of programingLanguage");
+programingLanguage.forEach(language => console.log (language));
